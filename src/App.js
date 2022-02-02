8dbe9a4152d6a5c4a99465fb9b484a7ef9e61df6
Abhishek Kumar <67472833+AbhishekKumar2233@users.noreply.github.com>
2022-02-02 13:53:05 +0530

    first method

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import "./styles.css";
+import cars from "./data";
 
 export default function App() {
   return (
     <div className="app">
-      <h3>Hyy</h3>
+      <table border="2">
+        <tr>
+          <th>Brand</th>
+          <th>Top Speed</th>
+          <th>Top Colours</th>
+        </tr>
+        <tr>
+          <td>{cars[0].model}</td>
+          <td>{cars[0].speedStats.topSpeed}</td>
+          <td>{cars[0].coloursByPropularity}</td>
+        </tr>
+      </table>
     </div>
   );
 }
